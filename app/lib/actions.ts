a6bf99fdcb28ecf142cea440a7f94cbe60730dc1
Arnaud Leray <arnaud.leray@niji.fr>
2025-05-09 12:04:53 +0200

    add form validation with Zod to createInvoice function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,17 @@
 'use server';
 
+import { z } from 'zod';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
     // const rawFormData = {
     //     customerId: formData.get('customerId'),
@@ -12,7 +24,9 @@ export async function createInvoice(formData: FormData) {
         customerId,
         amount,
         status,
-    } = Object.fromEntries(formData.entries());
+    } = CreateInvoice.parse(Object.fromEntries(formData.entries()));
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
-    console.log({ customerId, amount, status });
+    console.log({ customerId, amountInCents, status, date });
 }
