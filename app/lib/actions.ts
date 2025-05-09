076d6e132edf2a72085aa8cd0817a1e1f7c21d8c
Arnaud Leray <arnaud.leray@niji.fr>
2025-05-09 11:59:19 +0200

    create invoice page and form

---
+++ app/lib/actions.ts
@@ -0,0 +1,18 @@
+'use server';
+
+export async function createInvoice(formData: FormData) {
+    // const rawFormData = {
+    //     customerId: formData.get('customerId'),
+    //     amount: formData.get('amount'),
+    //     status: formData.get('status'),
+    // };
+    // console.log(rawFormData);
+
+    const {
+        customerId,
+        amount,
+        status,
+    } = Object.fromEntries(formData.entries());
+
+    console.log({ customerId, amount, status });
+}
